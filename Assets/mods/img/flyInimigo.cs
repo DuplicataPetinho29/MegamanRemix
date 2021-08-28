4d7f54beaffce7dadbd6633ce091c1662591ab7a
Matheus <Matheusfsantos.152@gmail.com>
2021-08-28 14:56:33 -0300

    dragão vira e personagem não diminui

--- Assets/mods/img/flyInimigo.cs
+++ Assets/mods/img/flyInimigo.cs
@@ -7,6 +7,14 @@ public class flyInimigo : MonoBehaviour
 
     private ControlaPer thePlayer;
 
+    [SerializeField]
+    Transform player;
+
+    [SerializeField]
+    float agroRange;
+
+    Rigidbody2D rigidbody2d;
+
     public float moveSpeed;
 
     public float playerRange;
@@ -19,16 +27,33 @@ public class flyInimigo : MonoBehaviour
     void Start()
     {
         thePlayer = FindObjectOfType<ControlaPer>();
+
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, thePlayer.transform.position, moveSpeed * Time.deltaTime);
+        chasePlayer();
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.DrawSphere(transform.position, playerRange);
+    }
+
+    void chasePlayer()
+    {
+        if (transform.position.x < player.position.x)
+        {
+            rigidbody2d.velocity = new Vector2(moveSpeed, 0);
+            transform.localScale = new Vector2(-3, 3);
+        }
+        else
+        {
+            rigidbody2d.velocity = new Vector2(-moveSpeed, 0);
+            transform.localScale = new Vector2(3, 3);
+        }
     }
 }
