260bbd371afa02a92d1389585e0d1a919c0a05d9
Matheus <matheusfsantos.152@gmail.com>
2021-09-01 11:02:40 -0300

    add vidas e cão

--- Assets/mods/img/caoInimigo.cs
+++ Assets/mods/img/caoInimigo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
-using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class flyInimigo : MonoBehaviour
+public class caoInimigo : MonoBehaviour
 {
 
     private ControlaPer thePlayer;
@@ -12,40 +12,40 @@ public class flyInimigo : MonoBehaviour
     [SerializeField]
     Transform player;
 
-    [SerializeField]
-    float agroRange;
-
-    Rigidbody2D rigidbody2d;
-
     public float moveSpeed;
 
     public float playerRange;
-
-    public bool dam;
 
     public LayerMask playerLayer;
 
     public bool playerInRange;
 
+    public bool dam;
+
+    Rigidbody2D rigidbody2d;
+
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         thePlayer = FindObjectOfType<ControlaPer>();
 
-        saude = FindObjectOfType<Health>();
-
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        animator = GetComponent<Animator>();
+
+        saude = FindObjectOfType<Health>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
         playerInRange = Physics2D.OverlapCircle(transform.position, playerRange, playerLayer);
 
         if (playerInRange)
         {
             playerInRange = true;
+            animator.SetBool("CORRENDO", true);
             moveSpeed = 1;
             transform.position = Vector3.MoveTowards(transform.position, thePlayer.transform.position, moveSpeed * Time.deltaTime);
             chasePlayer();
@@ -53,10 +53,12 @@ public class flyInimigo : MonoBehaviour
         }
         else
         {
-            //playerInRange = false;
+            animator.SetBool("CORRENDO", false);
             moveSpeed = 0;
             chasePlayer();
+            // dano();
         }
+        // dano();
     }
 
     private void OnDrawGizmosSelected()
@@ -64,29 +66,45 @@ public class flyInimigo : MonoBehaviour
         Gizmos.DrawSphere(transform.position, playerRange);
     }
 
+
+
+
     void chasePlayer()
     {
         if (transform.position.x < player.position.x)
         {
             rigidbody2d.velocity = new Vector2(moveSpeed, 0);
-            transform.localScale = new Vector2(-1, 1);
+            transform.localScale = new Vector2(1, 1);
         }
         else
         {
             rigidbody2d.velocity = new Vector2(-moveSpeed, 0);
-            transform.localScale = new Vector2(1, 1);
+            transform.localScale = new Vector2(-1, 1);
         }
     }
 
+    //  void OnTriggerEnter2D(Collider2D trig)
+    //  {
+    //      if (trig.gameObject.CompareTag("parede"))
+    //      {
+    //          animator.SetBool("correndo", false);
+    //
+    //      }
+    //      else
+    //      {
+    //          animator.SetBool("correndo", true);
+    //      }
+    //  }
+
     void dano()
-    {
-        if (transform.position.x == player.position.x)
-        {
-            dam = true;
-            player.position = new Vector3(-2f, -3f);
-            saude.health = saude.health - 1;
+     {
+         if (transform.position.x == player.position.x)
+         {
+             dam = true;
+             player.position = new Vector3(-5.2f, -0, 24f);
+             saude.health = saude.health - 1;
         }
-        else
-            dam = false;
-    }
+         else
+             dam = false;
+     }
 }
