ee193bd2c0db819c27f1dfeedc6bc43cd345caed
Matheus <matheusfsantos.152@gmail.com>
2021-09-03 11:52:24 -0300

    atualiza cão

--- Assets/mods/img/caoInimigo.cs
+++ Assets/mods/img/caoInimigo.cs
@@ -7,10 +7,23 @@ public class caoInimigo : MonoBehaviour
 
     private ControlaPer thePlayer;
 
-    private Health saude;
-
     [SerializeField]
     Transform player;
+
+    [SerializeField]
+    Transform GroundCheckPoint;
+
+    [SerializeField]
+    Transform WallCheckPoint;
+
+    [SerializeField]
+    float circleRadius;
+
+    [SerializeField]
+    LayerMask GroundLayer;
+
+    private bool checkingGround;
+    private bool chekingWall;
 
     public float moveSpeed;
 
@@ -33,23 +46,18 @@ public class caoInimigo : MonoBehaviour
         rigidbody2d = GetComponent<Rigidbody2D>();
 
         animator = GetComponent<Animator>();
-
-        saude = FindObjectOfType<Health>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerInRange = Physics2D.OverlapCircle(transform.position, playerRange, playerLayer);
-
         if (playerInRange)
         {
             playerInRange = true;
-            animator.SetBool("CORRENDO", true);
-            moveSpeed = 1;
+            barra();
             transform.position = Vector3.MoveTowards(transform.position, thePlayer.transform.position, moveSpeed * Time.deltaTime);
             chasePlayer();
-            dano();
+            moveSpeed = 1;
         }
         else
         {
@@ -61,9 +69,44 @@ public class caoInimigo : MonoBehaviour
         // dano();
     }
 
+    private void FixedUpdate() //
+    {
+
+        playerInRange = Physics2D.OverlapCircle(transform.position, playerRange, playerLayer);
+
+        checkingGround = Physics2D.OverlapCircle(GroundCheckPoint.position, circleRadius, GroundLayer);
+        chekingWall = Physics2D.OverlapCircle(WallCheckPoint.position, circleRadius, GroundLayer);
+    }
+
+
     private void OnDrawGizmosSelected()
     {
-        Gizmos.DrawSphere(transform.position, playerRange);
+        Gizmos.DrawWireSphere(transform.position, playerRange);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(GroundCheckPoint.position, circleRadius);
+        Gizmos.DrawSphere(WallCheckPoint.position, circleRadius);
+
+    }
+
+    void barra()
+    {
+        if (!checkingGround)
+        {
+            animator.SetBool("CORRENDO", false);
+            moveSpeed = 0;
+        }
+        else
+        {
+            animator.SetBool("CORRENDO", true);
+            moveSpeed = 1;
+        }
+        if (chekingWall)
+        {
+            animator.SetTrigger("PULA");
+            rigidbody2d.AddForce(Vector2.up * 800f);
+        }
+
+
     }
 
 
@@ -83,28 +126,16 @@ public class caoInimigo : MonoBehaviour
         }
     }
 
-    //  void OnTriggerEnter2D(Collider2D trig)
-    //  {
-    //      if (trig.gameObject.CompareTag("parede"))
-    //      {
-    //          animator.SetBool("correndo", false);
-    //
-    //      }
-    //      else
-    //      {
-    //          animator.SetBool("correndo", true);
-    //      }
-    //  }
 
-    void dano()
-     {
-         if (transform.position.x == player.position.x)
-         {
-             dam = true;
-             player.position = new Vector3(-5.2f, -0, 24f);
-             saude.health = saude.health - 1;
-        }
-         else
-             dam = false;
-     }
+
+    // void dano()
+    // {
+    //     if (transform.position.x == player.position.x)
+    //     {
+    //         dam = true;
+    //         player.position = new Vector3(-5.2f, -0, 24f);
+    //     }
+    //     else
+    //         dam = false;
+    // }
 }
